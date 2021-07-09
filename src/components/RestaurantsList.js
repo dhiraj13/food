253014265e21d737153843fd1c727f74f9400acd
Dhiraj Khapangi <dkhapangi13@outlook.com>
2021-07-09 18:29:50 +0545

    flatlist rendering

--- src/components/RestaurantsList.js
+++ src/components/RestaurantsList.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
 
 const RestaurantsList = ({ title, restaurants }) => {
   return (
     <View>
       <Text style={styles.titleStyle}>{title}</Text>
-      <Text>Restaurants: {restaurants.length}</Text>
+      <FlatList
+        horizontal
+        data={restaurants}
+        keyExtractor={(restaurant) => restaurant.id}
+        renderItem={({ item }) => {
+          return <Text>{item.name}</Text>
+        }}
+      />
     </View>
   )
 }
