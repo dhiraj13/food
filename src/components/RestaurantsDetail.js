6212a452fef75aa4bd3c23e4bd5695aacbcefd01
Dhiraj Khapangi <dkhapangi13@outlook.com>
2021-07-09 22:42:03 +0545

    showing additional info

--- src/components/RestaurantsDetail.js
+++ src/components/RestaurantsDetail.js
@@ -4,11 +4,24 @@ import { View, Image, Text, StyleSheet } from 'react-native'
 const RestaurantsDetail = ({ restaurant }) => {
   return (
     <View>
-      <Text>{restaurant.name}</Text>
+      <Image style={styles.imageStyle} source={{ uri: restaurant.image_url }} />
+      <Text style={styles.nameStyle}>{restaurant.name}</Text>
+      <Text>
+        {restaurant.rating} Stars, {restaurant.review_count} Reviews
+      </Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  imageStyle: {
+    width: 250,
+    height: 120,
+    borderRadius: 4,
+  },
+  nameStyle: {
+    fontWeight: 'bold',
+  },
+})
 
 export default RestaurantsDetail
