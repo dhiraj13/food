0e85b3a964a27efcf34348425ee784ba31d8e0ca
Dhiraj Khapangi <dkhapangi13@outlook.com>
2021-07-09 18:07:44 +0545

    extracting the hooks login

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,32 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import SearchBar from '../components/SearchBar'
-import yelp from '../api/yelp'
+import useRestaurants from '../hooks/useRestaurants'
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('')
-  const [restaurants, setRestaurants] = useState([])
-  const [errorMessage, setErrorMessage] = useState('')
-
-  const searchApi = async (searchTerm) => {
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: 'san jose',
-        },
-      })
-      setRestaurants(response.data.businesses)
-      setErrorMessage('')
-    } catch (err) {
-      setErrorMessage('Something went wrong')
-    }
-  }
-
-  useEffect(() => {
-    searchApi('pasta')
-  }, [])
+  const [searchApi, restaurants, errorMessage] = useRestaurants()
 
   return (
     <View>
