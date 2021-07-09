53ce0fbce1199f54d5b81f5c252179fe6e7f3866
Dhiraj Khapangi <dkhapangi13@outlook.com>
2021-07-09 23:22:18 +0545

    the withNavigation helper

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -4,7 +4,7 @@ import SearchBar from '../components/SearchBar'
 import useRestaurants from '../hooks/useRestaurants'
 import RestaurantsList from '../components/RestaurantsList'
 
-const SearchScreen = ({ navigation }) => {
+const SearchScreen = () => {
   const [term, setTerm] = useState('')
   const [searchApi, restaurants, errorMessage] = useRestaurants()
 
@@ -27,17 +27,14 @@ const SearchScreen = ({ navigation }) => {
         <RestaurantsList
           restaurants={filterRestaurantsByPrice('$')}
           title='Cost Effective'
-          navigation={navigation}
         />
         <RestaurantsList
           restaurants={filterRestaurantsByPrice('$$')}
           title='Bit Pricier'
-          navigation={navigation}
         />
         <RestaurantsList
           restaurants={filterRestaurantsByPrice('$$$')}
           title='Big Spender'
-          navigation={navigation}
         />
       </ScrollView>
     </>
